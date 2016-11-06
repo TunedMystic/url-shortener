bbf84c71e2dab72ca0ec487a76bae39928ae7ba1
gitsomecode <sjbitcode@gmail.com>
2016-11-05 21:51:43 -0400

    Create tools for Django shell and managing the database.

---
+++ scripts/db.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+import os
+import sys
+
+DB_NAME = 'db.sqlite3'
+DUMP = 'sqlite3 {} .dump > {}'
+RESTORE = 'sqlite3 {} < {}'
+
+
+def write(s):
+    sys.stdout.write(s)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        '-r'
+        '--restore',
+        action='store_true',
+        dest='restore',
+        default=False,
+        help='Restore a database.'
+    )
+
+    parser.add_argument(
+        '-d'
+        '--dump',
+        action='store_true',
+        dest='dump',
+        default=False,
+        help='Dump a database.'
+    )
+
+    parser.add_argument(
+        '-f'
+        '--file',
+        action='store',
+        dest='filename',
+        default=None,
+        help='Filename of database.'
+    )
+
+    args = parser.parse_args()
+
+    # Check if a database action is passed.
+    if not (args.dump or args.restore):
+        write('Please specifiy a dump (-d) or restore (-r) option.\n')
+        sys.exit(1)
+
+    # Check if a filename is passed.
+    if not args.filename:
+        write('Please specify a filename (-f).\n')
+        sys.exit(1)
+
+    # Dump the database.
+    if args.dump:
+        cmd = DUMP.format(DB_NAME, args.filename)
+        write('Dumping database with:  {}\n'.format(cmd))
+        os.system(cmd)
+
+        sys.exit()
+
+    # Restore the database.
+    if args.restore:
+        # Delete the existing database.
+        write('Deleting existing database {}\n'.format(DB_NAME))
+        try:
+            os.remove(DB_NAME)
+        except (FileNotFoundError):
+            write('File {} not found \n'.format(DB_NAME))
+
+        # Restore the database.
+        cmd = RESTORE.format(DB_NAME, args.filename)
+        write('Restoring database with: {}\n'.format(cmd))
+        os.system(cmd)
+
+        sys.exit()
---
+++ scripts/django-shell.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+import django
+
+
+def print_header():
+    header = """
+      _   _                                    _            _  _ 
+   __| | (_)  __ _  _ __    __ _   ___    ___ | |__    ___ | || |
+  / _` | | | / _` || '_ \  / _` | / _ \  / __|| '_ \  / _ \| || |
+ | (_| | | || (_| || | | || (_| || (_) | \__ \| | | ||  __/| || |
+  \__,_|_/ | \__,_||_| |_| \__, | \___/  |___/|_| |_| \___||_||_|
+       |__/                |___/                                 
+    """
+    print(header)
+    print("")
+
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.abspath('/Users/sangeeta/Code/url-shortener/main'))
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+    django.setup()
+
+    # Import models.
+    from users.models import *
+    from links.models import *
+
+    # Easy model functions.
+    uu = lambda: User.objects.all()
+    ll = lambda: Link.objects.all()
+
+    print_header()
