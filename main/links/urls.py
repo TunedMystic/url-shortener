838d41bd164136a5fa991ef4cf40321ba6de9835
gitsomecode <sjbitcode@gmail.com>
2016-11-07 00:02:20 -0500

    Add redirect_url view to permanently redirect to a link's destination url

--- main/links/urls.py
+++ main/links/urls.py
@@ -3,6 +3,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^shorten-url/$', views.shorten_url, name='shorten-url'),
+    url(
+        r'^$',
+        views.index,
+        name='index'
+    ),
+    url(
+        r'^shorten-url/$',
+        views.shorten_url,
+        name='shorten-url'
+    ),
+    url(
+        r'^(?P<key>[\w-]+)/$',
+        views.redirect_url,
+        name='redirect-url'
+    ),
 ]
--- main/links/views.py
+++ main/links/views.py
@@ -1,9 +1,11 @@
 from django.contrib.sites.models import Site
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.db.models import F
+from django.http import JsonResponse, HttpResponsePermanentRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_http_methods
 
 from .forms import LinkForm
+from .models import Link
 
 
 def index(request):
@@ -25,3 +27,13 @@ def shorten_url(request):
     else:
         errors = form.errors
         return JsonResponse(errors, status=400)
+
+
+def redirect_url(request, key):
+    link = get_object_or_404(Link, key=key)
+
+    # Update Link total clicks.
+    link.total_clicks = F('total_clicks') + 1
+    link.save()
+
+    return HttpResponsePermanentRedirect(link.destination)
