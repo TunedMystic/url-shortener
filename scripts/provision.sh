43445fbb0411797683e5e9abe93b5fa01fcfd197
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-11-13 23:36:04 -0500

    Add nginx to the provision.sh script.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,6 +6,7 @@ sudo apt-get -y upgrade
 
 sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-pip
+sudo apt-get install -y nginx
 sudo apt-get install -y tree
 sudo apt-get install -y screen
 
