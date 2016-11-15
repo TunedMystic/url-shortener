39a73c99962392bf7717dc47f961fac6a3b1a8c6
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-11-14 21:54:36 -0500

    Add sqlite3 to provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -7,6 +7,7 @@ sudo apt-get -y upgrade
 sudo apt-get install -y python3-dev
 sudo apt-get install -y python3-pip
 sudo apt-get install -y nginx
+sudo apt-get install -y sqlite3
 sudo apt-get install -y tree
 sudo apt-get install -y screen
 
