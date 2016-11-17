fd9184aa9964c99f39a6b3867e35a27bab980456
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-11-16 20:20:48 -0500

    Modify the regex for the 'edit_link' and 'redirect_to_link' views. Make the regex accept only alphanumeric characters and dashes.

--- main/links/urls.py
+++ main/links/urls.py
@@ -19,12 +19,12 @@ urlpatterns = [
         name='shorten-link'
     ),
     url(
-        r'^edit/(?P<key>[\w-]+)/$',
+        r'^edit/(?P<key>[(A-Za-z0-9)-]++)/$',
         views.edit_link,
         name='edit-link'
     ),
     url(
-        r'^(?P<key>[\w-]+)/$',
+        r'^(?P<key>[(A-Za-z0-9)-]+)/$',
         views.redirect_to_link,
         name='redirect-to-link'
     ),
