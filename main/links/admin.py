90643bcf3263f2a82a8234973539fe0015ca4724
Sandeep Jadoonanan <jsanweb@gmail.com>
2016-11-26 02:53:07 -0500

    Show unique addresses in Link Admin.

--- main/links/admin.py
+++ main/links/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 from .models import Link, Tag
 
+
+class LinkAdmin(admin.ModelAdmin):
+    readonly_fields = ('unique_addresses',)
+
+    def unique_addresses(self, instance):
+
+        unique_ips = instance.addresses.count()
+        if unique_ips >= 50:
+            return '{} Unique IP Addresses'.format(unique_ips)
+
+        return '\n'.join(address.address for address in instance.addresses.all())
+
 # Register your models here.
-admin.site.register(Link)
+admin.site.register(Link, LinkAdmin)
 admin.site.register(Tag)
