7e9ef584bf580b079f1d0c63892db83d9478393b
gitsomecode <sjbitcode@gmail.com>
2016-12-02 00:49:23 -0500

    Add static url settings to project urls

--- main/config/urls.py
+++ main/config/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
@@ -27,7 +28,7 @@ if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
+    ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 # root_urlconf = __import__(settings.ROOT_URLCONF)
